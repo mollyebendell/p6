e17b5293708b12bf6f480f9d9d5632a9026fba44
mollyebendell <mbendell1@gmail.com>
2018-05-14 15:53:16 -0400

    Add files via upload

---
+++ triangles.js
@@ -0,0 +1,38 @@
+function setup() {
+    // put setup code here
+    createCanvas(800, 600);
+    background(120, 120, 255);
+    noStroke();
+    fill(0, 40, 200);
+    triangle(100, 50, 300, 50, 200, 550);
+    fill(200, 200, 255);
+    ellipse(200, 150, 100, 100);
+    fill(0, 0, 100);
+    triangle(200, 150, 350, 350, 50, 350);
+    fill(10, 10, 150);
+    stroke(110, 110, 255);
+    strokeWeight(3);
+    ellipse(200, 175, 75, 75);
+    noStroke();
+    ellipse(400, 400, 25, 25);
+    ellipse(400, 275, 75, 75);
+    ellipse(400, 100, 150, 150);
+    stroke(190, 75, 0);
+    strokeWeight(4);
+    line(475, 350, 750, 350);
+    line(300, 150, 750, 150);
+    noStroke();
+    fill(200, 0, 0);
+    triangle(525, 100, 700, 100, 700, 550);
+    fill(125, 0, 0);
+    triangle(525, 200, 650, 125, 725, 500);
+    fill(100, 0, 0);
+    triangle(525, 300, 600, 200, 750, 450);
+    fill(0);
+    triangle(550, 400, 500, 575, 600, 575);
+    triangle(550, 375, 475, 500, 625, 500);
+}
+
+function draw() {
+    // put drawing code here
+}
